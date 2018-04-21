91c870438b5c9f08382dedb56ba9b1f2d9d26936
Caluian Iulian <iulian.caluian@my.fmi.unibuc.ro>
2018-04-21 15:52:37 +0300

    Set up firebase for project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDFYsG67g4-f1lDVmS0qLPzHi_cNBUXK2k",
+    authDomain: "pitch-it-140f4.firebaseapp.com",
+    databaseURL: "https://pitch-it-140f4.firebaseio.com",
+    projectId: "pitch-it-140f4",
+    storageBucket: "",
+    messagingSenderId: "979560439757"
+  }
 };
